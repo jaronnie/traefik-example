782bd141a35d69b31e25b3e462f03e3c5723f3a8
jaronnie <jaron@jaronnie.com>
2023-07-14 11:39:10 +0800

    feat(traefik): update project example

--- project/apps/app3/main.go
+++ project/apps/app3/main.go
@@ -4,11 +4,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Data struct {
+	Message string `json:"message"`
+}
+
 func main() {
 	e := gin.Default()
 
-	e.GET("/api/v1/echo", func(ctx *gin.Context) {
-		ctx.String(200, "hello, app3")
+	e.GET("/api/v1/echo/:message", func(ctx *gin.Context) {
+		message := ctx.Param("message")
+		ctx.String(200, "hello app3: "+message)
+	})
+
+	e.POST("/api/v1/post", func(ctx *gin.Context) {
+		var d Data
+		err := ctx.BindJSON(&d)
+		if err != nil {
+			ctx.String(500, "bind Data err: "+err.Error())
+		}
+		ctx.String(200, "post message: "+d.Message)
 	})
 
 	e.Run(":9000")
